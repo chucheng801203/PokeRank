cdb975de5c77409081adcfa506c6c94c5f3f9226
Peter Chu <chucheng801203@gmail.com>
2021-09-30 12:34:36 +0800

    新增排程

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('pokemon:update-rank --season=latest')->dailyAt('5:00');
+        $schedule->command('pokemon:update-data')->monthlyOn(1, '14:00');
+        $schedule->command('pokemon:download-images')->monthlyOn(1, '14:05');
     }
 
     /**
