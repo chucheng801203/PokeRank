da06a7c8af65f3e25f4728ecab3ffb670f2f2f26
peterchu <chucheng801203@gmail.com>
2021-11-14 23:02:39 +0800

    修改資料表欄位格式及註解

--- crawler/database/migrations/2021_01_05_122344_create_rank_nature_table.php
+++ crawler/database/migrations/2021_01_05_122344_create_rank_nature_table.php
@@ -20,13 +20,14 @@ class CreateRankNatureTable extends Migration
         Capsule::schema()->create($tableName, function (Blueprint $table) {
             $table->unsignedSmallInteger('pf_id')->nullable(true)->comment('pokemon 型態 id');
             $table->unsignedTinyInteger('season_number')->nullable(true)->comment('賽季');
-            $table->string('rule', 6)->comment('single=單打, double=雙打');
+            $table->boolean('rule')->comment('0=單打, 1=雙打');
             $table->unsignedSmallInteger('nature_id')->nullable(true)->comment('個性 id');
             $table->decimal('percentage', $precision = 5, $scale = 2);
             $table->unsignedTinyInteger('sort')->comment('排序');
             $table->timestampsTz($precision = 0);
             $table->index('pf_id');
             $table->index('nature_id');
+            $table->index('season_number');
             $table->foreign('pf_id')->references('id')->on('pokeform')->onUpdate('cascade')->onDelete('SET NULL');
             $table->foreign('nature_id')->references('id')->on('nature')->onUpdate('cascade')->onDelete('SET NULL');
             $table->foreign('season_number')->references('season')->on('rank_season_list')->onUpdate('cascade')->onDelete('SET NULL');
--- crawler/database/migrations/2021_01_05_190850_create_rank_win_pokemon_table.php
+++ crawler/database/migrations/2021_01_05_190850_create_rank_win_pokemon_table.php
@@ -20,12 +20,13 @@ class CreateRankWinPokemonTable extends Migration
         Capsule::schema()->create($tableName, function (Blueprint $table) {
             $table->unsignedSmallInteger('pf_id')->nullable(true)->comment('pokemon 型態 id');
             $table->unsignedTinyInteger('season_number')->nullable(true)->comment('賽季');
-            $table->string('rule', 6)->comment('single=單打, double=雙打');
+            $table->boolean('rule')->comment('0=單打, 1=雙打');
             $table->unsignedSmallInteger('team_pf_id')->nullable(true)->comment('同隊 pokemon 型態 id');
             $table->unsignedTinyInteger('sort')->comment('排序');
             $table->timestampsTz($precision = 0);
             $table->index('pf_id');
             $table->index('team_pf_id');
+            $table->index('season_number');
             $table->foreign('pf_id')->references('id')->on('pokeform')->onUpdate('cascade')->onDelete('SET NULL');
             $table->foreign('team_pf_id')->references('id')->on('pokeform')->onUpdate('cascade')->onDelete('SET NULL');
             $table->foreign('season_number')->references('season')->on('rank_season_list')->onUpdate('cascade')->onDelete('SET NULL');
