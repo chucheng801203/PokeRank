596afe0b3e89b98813743380efdc8fbea5ca06e3
peterchu <chucheng801203@gmail.com>
2021-11-15 14:10:30 +0800

    修改錯誤訊息

--- crawler/app/Services/Pokemon/Exceptions/PokemonException.php
+++ crawler/app/Services/Pokemon/Exceptions/PokemonException.php
@@ -4,4 +4,9 @@ namespace App\Services\Pokemon\Exceptions;
 
 class PokemonException extends \Exception
 {
+    public function __construct(string $err_msg = '')
+    {
+        $err_msg = 'PokemonException: ' . ($err_msg !== '' ? $err_msg : '發生錯誤');
+        parent::__construct($err_msg);
+    }
 }
--- crawler/app/Services/Pokemon/Exceptions/PokemonFormatException.php
+++ crawler/app/Services/Pokemon/Exceptions/PokemonFormatException.php
@@ -4,9 +4,9 @@ namespace App\Services\Pokemon\Exceptions;
 
 class PokemonFormatException extends \Exception
 {
-    public function __construct($err_msg = null)
+    public function __construct(string $err_msg = '')
     {
-        $err_msg = $err_msg !== null ? $err_msg : '資料格式錯誤';
+        $err_msg = 'PokemonFormatException: ' . ($err_msg !== '' ? $err_msg : '資料格式錯誤');
         parent::__construct($err_msg);
     }
 }
