198eba13db684b1b53e95844445ce71f4cd4c621
peterchu <chucheng801203@gmail.com>
2021-11-28 21:09:19 +0800

    修改 hitory 初始化

--- src/containers/HistoryContainer.tsx
+++ src/containers/HistoryContainer.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useContext } from "react";
-import { useDispatch, useStore } from "react-redux";
-import { useHistory, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 import toggleRule from "../redux/actions/rule";
 import toggleSeason from "../redux/actions/season";
 import searchTextAction from "../redux/actions/searchText";
 import { RuleState } from "../redux/reducers/rule";
 import { SeasonState } from "../redux/reducers/season";
 import PageDataContext from "../contexts/PageDataContext";
-import { getDefaultState, RootState } from "../redux/store";
 
 export type HistoryState = {
     rule: RuleState;
@@ -19,38 +18,19 @@ const HistoryContainer: React.FC = () => {
     const pageData = useContext(PageDataContext);
     const dispatch = useDispatch();
 
-    const store = useStore<RootState>();
-    const { season } = store.getState();
-
-    const history = useHistory();
-    const { state, search, pathname } = useLocation<HistoryState>();
+    const { state } = useLocation<HistoryState>();
 
     // push & popstate & replace event
     useEffect(() => {
         if (!state || pageData.page_loading) return;
 
-        const defaultState = getDefaultState(pageData);
+        dispatch(toggleSeason(state.season));
 
-        if (
-            state.season.index === season[0].index &&
-            defaultState.season[0].value !== season[0].value &&
-            search === ""
-        ) {
-            // 當賽季更新時，重新整理時首頁的 location state 也要更新成最新賽季
-            history.replace(pathname + search, {
-                rule: defaultState.rule[0],
-                season: defaultState.season[0],
-                searchText: "",
-            });
-        } else {
-            dispatch(toggleSeason(state.season));
+        dispatch(toggleRule(state.rule));
 
-            dispatch(toggleRule(state.rule));
-
-            dispatch(
-                searchTextAction(state.searchText ? state.searchText : "")
-            );
-        }
+        dispatch(
+            searchTextAction(state.searchText ? state.searchText : "")
+        );
     });
 
     return null;
