bf247b0dae093a8ed6e1fbffd53049b9975f1151
peterchu <chucheng801203@gmail.com>
2021-12-25 13:26:01 +0800

    修改 HistoryContainer 條件

--- src/containers/HistoryContainer.tsx
+++ src/containers/HistoryContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useStore } from "react-redux";
 import { useLocation } from "react-router-dom";
 import toggleRule from "../redux/actions/rule";
 import toggleSeason from "../redux/actions/season";
@@ -7,6 +7,7 @@ import searchTextAction from "../redux/actions/searchText";
 import { RuleState } from "../redux/reducers/rule";
 import { SeasonState } from "../redux/reducers/season";
 import PageDataContext from "../contexts/PageDataContext";
+import { RootState } from "../redux/store";
 
 export type HistoryState = {
     rule: RuleState;
@@ -19,14 +20,20 @@ const HistoryContainer: React.FC = () => {
     const dispatch = useDispatch();
 
     const { state } = useLocation<HistoryState>();
+    const store = useStore<RootState>();
+    const {season, rule} = store.getState();
 
     // push & popstate & replace event
     useEffect(() => {
         if (!state || pageData.page_loading) return;
 
-        dispatch(toggleSeason(state.season));
+        if (season[0].index !== state.season.index) {
+            dispatch(toggleSeason(state.season));
+        }
 
-        dispatch(toggleRule(state.rule));
+        if (rule[0].index !== state.rule.index) {
+            dispatch(toggleRule(state.rule));
+        }
 
         dispatch(searchTextAction(state.searchText ? state.searchText : ""));
     });
